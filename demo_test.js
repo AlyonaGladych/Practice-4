e1c16ba9922029afbfab2419e223b70f34cce22f
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-04 10:57:41 +0300

    Create demo_test.js

---
+++ demo_test.js
@@ -0,0 +1,15 @@
+let LoginPage = require("../page_objects/login_page.js");
+
+describe('login suite', function() {
+    it('login pass test', async function() {
+      let loginPage = new LoginPage();
+
+      await loginPage.open();
+      let productsPage = await loginPage.login("alyonagladych@gmail.com", "x&b*zmzA^oj");
+
+      console.log(productsPage.header + '####################################');
+      await browser.sleep(3000);
+  
+     expect(await productsPage.header.isHeaderVisible()).toEqual(true);
+    });
+});
