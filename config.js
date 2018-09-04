a581229f6233b2b139c7e3c5f4e84391d6ed3ac1
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-04 21:13:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,13 +22,22 @@ exports.config = {
       defaultTimeoutInterval: 45000
     },
 
-   exports.config = {
     framework: 'jasmine2',
+
     onPrepare: function() {
-        var AllureReporter = require('jasmine-allure-reporter');
-        jasmine.getEnv().addReporter(new AllureReporter({
-            resultsDir: 'allure-results'
-    }));
-  }
-}
+      let AllureReporter = require('jasmine-allure-reporter');
+      jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+      }));
+      jasmine.getEnv().afterEach(function(done){
+        browser.takeScreenshot().then(function (png) {
+          allure.createAttachment('Screenshot', function () {
+            return new Buffer(png, 'base64')
+          }, 'image/png')();
+          done();
+        })
+      });
+
+    }
+  };
   
