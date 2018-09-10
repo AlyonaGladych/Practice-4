1e808642a35762c110ea6f2d7ef4b11edb48fc8d
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-10 22:51:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,43 +1,45 @@
 // An example configuration file
 exports.config = {
-    // The address of a running selenium server.
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    SELENIUM_PROMISE_MANAGER: 0,
-    allScriptsTimeout: 30,
-  
-    // Capabilities to be passed to the webdriver instance.
-    capabilities: {
-      browserName: 'chrome',
-      maxInstances: 1
-    },
-  
-    // Spec patterns are relative to the configuration file location passed
-    // to protractor (in this example conf.js).
-    // They may include glob patterns.
-    specs: ['./tests/demo_test.js'],
-  
-    // Options to be passed to Jasmine-node.
-    jasmineNodeOpts: {
-      showColors: true, // Use colors in the command line report.
-      defaultTimeoutInterval: 45000
-    },
+  // The address of a running selenium server.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  SELENIUM_PROMISE_MANAGER: 0,
+  allScriptsTimeout: 30,
 
-    framework: 'jasmine2',
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    browserName: 'chrome',
+    maxInstances: 1
+  },
 
-    onPrepare: function() {
-      let AllureReporter = require('jasmine-allure-reporter');
-      jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'allure-results'
-      }));
-      jasmine.getEnv().afterEach(function(done){
-        browser.takeScreenshot().then(function (png) {
-          allure.createAttachment('Screenshot', function () {
-            return new Buffer(png, 'base64')
-          }, 'image/png')();
-          done();
-        })
-      });
+  // Spec patterns are relative to the configuration file location passed
+  // to protractor (in this example conf.js).
+  // They may include glob patterns.
+  specs: ['./tests/demo_test2.js', './tests/demo_test3.js'],
 
-    }
-  };
-  
+  restartBrowserBetweenTests: true,
+
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true, // Use colors in the command line report.
+    defaultTimeoutInterval: 45000
+  },
+
+  framework: 'jasmine2',
+
+  onPrepare: function() {
+    let AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+    resultsDir: 'allure-results'
+    }));
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+
+  }
+};
